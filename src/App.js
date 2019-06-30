5befb373b1ec57fc0334a9a7ef16add2ff74523c
bilgeyucel <bilgeyucel@sabanciuniv.edu>
2019-06-30 21:13:31 +0300

    middle of hooks

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
+// import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement(
-    "div", //what kind of tag it is
-    { id: "something-important" }, // attributes we can give
-    React.createElement("h1", {}, "Adopt-me!"), // children
-    React.createElement(Pet, { name: "Kedi", animal: "Cat", breed: "siyam" }),
-    React.createElement(Pet, { name: "Kopek", animal: "Dog", breed: "golden" }),
-    React.createElement(Pet, { name: "Kus", animal: "Bird", breed: "papagan" })
+  return (
+    <div>
+      <h1 id={"something-importatn"}>Adopt-me!</h1>
+      {/* <Pet name="Kedi" animal="Cat" breed="siyam" />
+      <Pet name="Kopek" animal="Dog" breed="golden" />
+      <Pet name="Kus" animal="Bird" breed="papagan" /> */}
+      <SearchParams />
+    </div>
   );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
 
 // ReactDOM.render(
 //     React.createElement(
--- src/Pet.js
+++ src/Pet.js
@@ -1,12 +1,21 @@
 import React from "react";
 
 export default function Pet({ name, animal, breed }) {
-  //{name, animal, breed} burada props aslida
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
+  // export const Pet = ({ name, animal, breed }) => {
+  // Bu da function olmasi yerine bir alternatif aslinda
+  // {name, animal, breed} burada props aslida
+  // return React.createElement(
+  //   "div",
+  //   {},
+  //   React.createElement("h1", {}, name),
+  //   React.createElement("h2", {}, animal),
+  //   React.createElement("h2", {}, breed)
+  // );
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{animal}</h2>
+      <h2>{breed}</h2>
+    </div>
   );
 }
