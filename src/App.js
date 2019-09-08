8e6bad77d3c63b05dfd75be15b852f8faa68cd37
bilgeyucel <bilgeyucel@sabanciuniv.edu>
2019-09-08 15:19:46 +0300

    Add Context and useContext

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
 import SearchParams from "./SearchParams";
 import { Router, Link } from "@reach/router";
 import Details from "./Details";
+import ThemeContext from "./ThemeContext";
 // import Pet from "./Pet";
 
 const App = () => {
+  const theme = useState("darkblue");
   return (
-    <div>
-      <header>
-        <Link to={"/ "}>Adopt-me!</Link>
-      </header>
-      <Router>
-        <SearchParams path={"/"} />
-        <Details path={"/details/:id"} />
-      </Router>
-    </div>
+    <ThemeContext.Provider value={theme}>
+      <div>
+        <header>
+          <Link to={"/ "}>Adopt-me!</Link>
+        </header>
+        <Router>
+          <SearchParams path={"/"} />
+          <Details path={"/details/:id"} />
+        </Router>
+      </div>
+    </ThemeContext.Provider>
   );
 };
 
